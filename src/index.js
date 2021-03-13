3498dc60b41693bd507bd98aa0e1b4d12aa6d876
Alexey Azarko <Azarkoav@gmail.com>
2021-03-13 22:29:15 +0300

    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n;
+    }
+  let str = String(n);
+  result = '';
+  for (let i = (str.length - 1); i >= 0; i--) {
+      result += str[i];
+  }
+  return Number(result);
 }
